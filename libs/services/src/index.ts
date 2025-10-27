a33a8f1a94d7a32a910ec7220dfd6094b3e00561
Nick <128822732+ToxicBlades@users.noreply.github.com>
2025-10-27 10:57:16 +0200

    feat: generate index and build from root package

---
+++ libs/services/src/index.ts
@@ -0,0 +1,6 @@
+export * from './types/action-type';
+export * from './types/common/StrapiQuery';
+export * from './types/common/base_type';
+export * from './types/common/paginationParams';
+export * from './api-routes/api-routes-composer';
+export * from './api-routes/api-routes-crm';
---
+++ libs/services/src/server.ts
@@ -0,0 +1,4 @@
+export * from './services/action-type.service';
+export * from './services/common/base.service';
+export * from './services/common/response.service';
+export * from './services/common/typeList';
